6d365cec623409e47b21aa49e7d4666aa7b86f41
velizcarolina <99051897+velizcarolina@users.noreply.github.com>
2023-09-08 14:03:18 -0400

    Add files via upload

--- Car/CarFactory.py
+++ Car/CarFactory.py
@@ -1,7 +1,11 @@
 from abc import ABC, abstractclassmethod
 from datetime import date
-from Battery import battery
-from Engine import engine
+from Car import Car
+from Battery.NubbinBattery import NubbinBattery
+from Battery.SpindlerBattery import SpindlerBattery
+from CapuletEngine import CapuletEngine
+from SternmanEngine import SternmanEngine
+from WilloughbyEngine import WilloughbyEngine
 
 class CarFactory:
 
@@ -13,20 +17,35 @@ class CarFactory:
 
     #Capulet, Splinder
     def create_calliope(current_date:date, last_service_date:date, current_milage:int, last_service_milage:int):
-        return
+        engine = CapuletEngine(last_service_milage, current_milage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        car = Car(engine, battery)
+        return car
     
     #Willoughby, Splinder
     def create_glissade(current_date:date, last_service_date:date, current_milage:int, last_service_milage:int):
-        return
+        engine = WilloughbyEngine(last_service_milage, current_milage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        car = Car(engine, battery)
+        return car
     
     #Sternman, Splinder
     def create_palindrome(current_date:date, last_service_date:date, warning_light_on:bool):
-        return
+        engine = SternmanEngine(warning_light_on)
+        battery = SpindlerBattery(last_service_date, current_date)
+        car = Car(engine, battery)
+        return car
     
     #Willoughby, Nubbin
     def create_rorschach(current_date:date, last_service_date:date, current_milage:int, last_service_milage:int):
-        return
+        engine = WilloughbyEngine(last_service_milage, current_milage)
+        battery = NubbinBattery(last_service_date, current_date)
+        car = Car(engine, battery)
+        return car
     
     #Capulet, Nubbin
     def create_thovax(current_date:date, last_service_date:date, current_milage:int, last_service_milage:int):
-        return
+        engine = CapuletEngine(last_service_milage, current_milage)
+        battery = NubbinBattery(last_service_date, current_date)
+        car = Car(engine, battery)
+        return car
